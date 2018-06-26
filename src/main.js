f04f83f7f041f4a024d7e003130add47ad81eab5
oniipon <oniipon@gmail.com>
2018-06-26 13:43:53 +0900

    とりあえずサイト表示出来る

--- src/main.js
+++ src/main.js
@@ -1,29 +1,38 @@
 "use strict";
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
-    result["default"] = mod;
-    return result;
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-const puppeteer = __importStar(require("puppeteer"));
-const URL = 'http://columbia.jp/idolmaster/';
+const puppeteer = require("puppeteer");
 (async () => {
-    const b = await puppeteer.launch();
-    const p = await b.newPage();
-    const b_options = {
+    const browser = await puppeteer.launch({ headless: false });
+    const page = await browser.newPage();
+    const b_option = {
         viewport: {
-            width: 1024,
-            height: 600,
+            width: 1920,
+            height: 1080,
         },
         userAgent: "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.101 Safari/537.36",
     };
-    await p.emulate(b_options);
-    await p.goto(URL);
-    const ss_options = {
-        path: '../',
-        type: "png",
+    page.emulate(b_option);
+    const goto_option = {
+        timeout: 0,
+        waitUntil: 'networkidle0'
     };
-    await p.screenshot(ss_options);
+    const start_time = new Date();
+    await page.goto('http://columbia.jp/idolmaster/', goto_option);
+    const end_time = new Date();
+    console.log(`レンダリング完了なう ${Date.parse(end_time.toString()) - Date.parse(start_time.toString())}`);
+    // // 検索窓に「puppeteer github」と入力
+    // await page.type('#lst-ib', 'puppeteer github');
+    // // 検索ボタンクリック
+    // await page.focus('input[name="btnK"]');
+    // await page.click('input[name="btnK"]');
+    // // 遷移完了を待機
+    // await page.waitForNavigation();
+    // // 検索結果の先頭リンクをクリック
+    // await page.click('.rc > .r > a');
+    // // ページタイトルを表示
+    // const title = await page.title();
+    // スクリーンショット
+    await page.screenshot({ path: './ss.png' });
+    await browser.close();
 })();
+//# sourceMappingURL=main.js.map
--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,30 @@
-import * as puppeteer from 'puppeteer'
-
-const URL= 'http://columbia.jp/idolmaster/';
+import * as puppeteer from 'puppeteer';
+import { NavigationOptions, EmulateOptions } from 'puppeteer';
 
 (async () => {
-  const b = await puppeteer.launch();
-  const p =await  b.newPage();
-  const b_options = {
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+  const b_option: Partial<EmulateOptions> = {
     viewport: {
-      width: 1024,
-      height: 600,
+      width: 1920,
+      height: 1080,
     },
     userAgent: "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.101 Safari/537.36",
-  };
-  await p.emulate(b_options)
-  await p.goto(URL)
-  const ss_options = {
-    path:'../',
-    type:'png',
   }
-  await p.screenshot(ss_options)
-})()
+  page.emulate(b_option)
+  const goto_option: Partial<NavigationOptions> = {
+    timeout: 0,
+    waitUntil: 'networkidle0'
+  }
+  const test_url = 'https://www.google.co.jp/'
+  const imas_url = 'http://columbia.jp/idolmaster/'
+  await page.goto(imas_url, goto_option)
+  const images: String[] = await page.evaluate(() => {
+    const imageList = document.querySelectorAll('img');
+    console.log('asijdaisojdaisopjij')
+    return Array.from(imageList).map(d => d.src);
+  })
+
+  await browser.close();
+})();
+
