4fee405260c17466ba220d82dfb529a1bf998d19
oniipon <oniipon@gmail.com>
2019-09-11 19:39:51 +0900

    画像保存方法変更

--- src/getImg.ts
+++ src/getImg.ts
@@ -1,15 +1,12 @@
 import * as fs from 'fs';
 import Axios from 'axios';
 import {Album} from './Album';
-const json: Album[][] = require('../Album.json');
+const json: Album[] = require('../Album.json');
 if (!fs.existsSync('./img')) {
   fs.mkdirSync('./img');
 }
 // 画像保存処理
-json.forEach(d => {
-  d.forEach(async j => {
-    const res = await Axios.get(j.url, { responseType: 'arraybuffer' });
-    const bf = Buffer.from(res.data);
-    fs.writeFileSync(`./img/${j.cd_title}.jpg`, Buffer.from(res.data), 'binary');
-  });
+json.forEach(async d => {
+    const res = await Axios.get(d.url, { responseType: 'arraybuffer' });
+    fs.writeFileSync(`./img/${d.cd_title}.jpg`, Buffer.from(res.data), 'binary');
 });
